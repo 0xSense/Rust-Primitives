835243619a0bb9d11902cff55bb17fb4d168acfd
Sense <105939929+0xSense@users.noreply.github.com>
2024-01-29 12:30:24 -0500

    chore: ownership section

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // stack & heap
+    // references
+    // there can only be one owner at a time
+    // rust is extremely scope dependent { foobar }
+    
+    let x  = String::from("Hello this is a basic string"); 
+    x_reference = &x;
+
 }
