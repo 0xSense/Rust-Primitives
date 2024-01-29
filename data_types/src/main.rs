17b79a9dfa1a86038e42d1add55d568c1bea690f
0xSense <105939929+0xSense@users.noreply.github.com>
2024-01-29 12:02:08 -0500

    build: creation of rust ownership directory

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -2,4 +2,12 @@
 // rust applications 
 fn main() {
   let guess: u32 = "42".parse().expect("Not a number");
+
+  let stuff = (10, 3.14, 'x');
+
+  say_hello();
+}
+
+fn say_hello() {
+  println!("Hello!")
 }
